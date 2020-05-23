037fcb96e8ac6553d330f1fd4a7d9f178acbc8af
Volkov Pavel <volkfm98@gmail.com>
2020-05-23 09:43:03 +0500

    Hotfix menu scene load

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -18,7 +18,7 @@ public class MenuManager : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("FirstLvl");
     }
     public void Exit()
     {
