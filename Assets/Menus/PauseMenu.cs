2c6253cdd4dc2ea4c4d5e4b06bba5f487317c195
Darya Sarapultceva <justdsarap@gmail.com>
2020-05-27 17:17:48 +0500

    Add pause menu on FirstLvl

---
+++ Assets/Menus/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	[SerializeField] private GameObject pauseMenuUI;
+
+	[SerializeField] private bool isPaused;
+
+    void Update()
+    {
+    	if(Input.GetKeyDown(KeyCode.Escape))
+    	{
+    		isPaused = !isPaused;
+    	}
+
+    	if(isPaused)
+    	{
+    		ActivateMenu();
+    	}
+    	else
+    	{
+    		DeactivateMenu();
+    	}
+    }
+
+    void ActivateMenu()
+    {	
+    	Time.timeScale = 0;
+    	pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+    	Time.timeScale = 1;
+    	pauseMenuUI.SetActive(false);
+    	isPaused = false;
+    }
+
+    public void Quit()
+    {
+    	Application.Quit();
+    }
+}
