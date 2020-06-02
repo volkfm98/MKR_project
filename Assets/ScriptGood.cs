aa7611b1310c8111c3aae8f88f645d46c8e71d74
KOSTyk <Kostya220198@mail.ru>
2020-06-02 12:29:17 +0500

    Даше <3

---
+++ Assets/ScriptGood.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptGood : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter (Collider col)
+    { if (col.tag == "Player")
+        { obj.SetActive(false); }            }
+}
