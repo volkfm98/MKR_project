7e63db8b3513a1e61a9e7ad93524c0b9b129d5f6
KOSTyk <Kostya220198@mail.ru>
2020-06-03 23:57:58 +0500

    платформа в движении+дыра в стене+ немного красоты.

---
+++ Assets/platform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class platform : MonoBehaviour
+{
+
+    float dirX;
+    float speed = 3f;
+
+    bool moveingUp = true;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 4f)
+        {
+            moveingUp = false;
+        }
+        else if (transform.position.y < -3f)
+        {
+            moveingUp = true;
+        }
+        if (moveingUp)
+        {
+            transform.position = new Vector2(transform.position.x , transform.position.y + speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
+        }
+    }
+}
