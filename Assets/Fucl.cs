35274c64f781390b7fc4ec8e1913dbc763e255ef
KOSTyk <Kostya220198@mail.ru>
2020-06-06 00:03:36 +0500

    Телепорт между уровнями готов! Но НЕ СЛИВАЙТЕ ЭТОТ КОММИТ С ДРУГИМИ

---
+++ Assets/Fucl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[AddComponentMenu ("My components/Fucl")]
+public class Fucl : MonoBehaviour
+{
+    [Header("Индекс сцены")]
+    public int sceneIndex;
+
+
+    void OnTriggerEnter2D (Collider2D mycollaider)
+    { 
+   if (mycollaider.tag == "Player")
+        { SceneManager.LoadScene(sceneIndex); }
+    }
+}
