32fec832f62124b19304377633662b8428658a7a
Darya Sarapultceva <justdsarap@gmail.com>
2020-06-06 06:02:53 +0500

    Fix after merge 2 lvl
    Added trigger which relocates player to lvl 2. Added pause menu on lvl 2.

---
+++ Assets/To2LVL.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class To2LVL : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		SceneManager.LoadScene("2_lvl");
+	}
+}
